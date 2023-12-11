5fc8f98f3d68190b423891407e5ddcbb8208af40
Ilin_Viacheslav <367255@niuitmo.ru>
2023-12-11 20:34:42 +0300

    slight changes

--- i.sh
+++ i.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
 startDate=$(date +"%F_%H:%M:%S")
-mkdir $HOME/test
-if [ $? -eq 0 ]; then
-	touch $HOME/test/"$startDate"
-	echo "catalog test was created successfully" > ~/report
-fi
-
-ping www.net_nikogo.ru
-if [ $? -ne 0 ]; then
-	currentDate=$(date +"%F_%H:%M:%S")
-	echo "$currentDate host is not available" >> ~/report
-fi
+mkdir $HOME/test && touch $HOME/test/"$startDate" && echo "catalog test was created successfully" > ~/report
+ping www.net_nikogo.ru || currentDate=$(date +"%F_%H:%M:%S") && echo "$currentDate host is not available" >> ~/report
